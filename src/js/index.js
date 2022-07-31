d4ea11fa22489ed0ad32f527edacdd42804b9011
Chukuwinke <rittnerodigwe@gmail.com>
2022-07-31 16:21:17 +0200

    added menu toggle funcionality

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+const hamburger = document.querySelector(".hamburger__menu");
+const menuContainer = document.querySelector(".menu__container")
+hamburgerIcon = document.querySelector(".hamburger-icon")
+
+hamburger.onclick = (e) => {
+    if(hamburgerIcon.classList.contains("fa-bars")){
+        hamburgerIcon.classList.remove("fa-bars")
+        hamburgerIcon.classList.add("fa-xmark")
+    }
+    else{
+        hamburgerIcon.classList.remove("fa-xmark")
+        hamburgerIcon.classList.add("fa-bars")
+    }
+    menuContainer.classList.toggle("active")
+}
